870cafc07c205fbd489a6b8c6b85ecfd844ecd33
isaackoi <67557111+isaackoi@users.noreply.github.com>
2026-05-24 12:18:24 +0100

    Use zero scroll reveal threshold

--- assets/js/page-enhancements.js
+++ assets/js/page-enhancements.js
@@ -8227,7 +8227,7 @@
     }, {
       root: null,
       rootMargin: "0px",
-      threshold: 0.1
+      threshold: 0
     });
 
     Array.prototype.forEach.call(animElements, function(el, index) {
